c258493758a79bbb388e305a1009435dbd7f5b46
D7x7z49 <85430783+D7x7z49@users.noreply.github.com>
2025-01-05 16:43:27 +0800

    fix: resolve merge conflicts and retain valuable parts; discard this branch

--- src/options/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TestComponent from '@workspacehub/components/TestComponent';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <TestComponent />
-  </React.StrictMode>,
-);
--- src/popup/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PopupPage from '@workspacehub/popup/PopupPage';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <PopupPage />
-  </React.StrictMode>,
-);
